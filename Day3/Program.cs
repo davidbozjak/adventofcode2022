82c59c556da75f80491701f7e9f0d92fe989e478
David Božjak <bozjak@gmail.com>
2022-12-03 06:15:13 +0100

    Added day 3

---
+++ Day3/Program.cs
@@ -0,0 +1,48 @@
+﻿var backpacks = new StringInputProvider("Input.txt").ToArray();
+
+int prioritySum = 0;
+
+foreach (var backpack in backpacks)
+{
+    int compartmentSize = backpack.Length / 2;
+    var compartment1 = backpack[..compartmentSize];
+    var compartment2 = backpack[compartmentSize..];
+
+    for (int i = 0; i < compartmentSize; i++)
+    {
+        var item = compartment1[i];
+        if (compartment2.Contains(item))
+        {
+            //found!
+            prioritySum += GetItemPriority(item);
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"Part 1: {prioritySum}");
+
+int badgePrioritySum = 0;
+
+for (int i = 0; i < backpacks.Length; i += 3)
+{
+    for (int itemIndex = 0; itemIndex < backpacks[i].Length; itemIndex++)
+    {
+        var item = backpacks[i][itemIndex];
+
+        if (backpacks[i + 1].Contains(item) && backpacks[i + 2].Contains(item))
+        {
+            //found
+            badgePrioritySum += GetItemPriority(item);
+            break;
+        }
+    }
+}
+
+Console.WriteLine($"Part 2: {badgePrioritySum}");
+
+int GetItemPriority(char item)
+{
+    int priority = item - (char.IsLower(item) ? 96 : 38);
+    return priority;
+}
