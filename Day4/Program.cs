4c243b45b42589044917db40c8f5ea7870a03584
David Božjak <bozjak@gmail.com>
2022-12-04 06:32:42 +0100

    Added day 4

---
+++ Day4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.RegularExpressions;
+
+var assignments = new InputProvider<AssignmentForPair?>("Input.txt", GetAssignmentForPair).Where(w => w != null).Cast<AssignmentForPair>().ToList();
+
+Console.WriteLine($"Part 1: {assignments.Count(w => w.IsOneFullyContainedByOther())}");
+Console.WriteLine($"Part 2: {assignments.Count(w => w.DoOverlapAtAll())}");
+
+static bool GetAssignmentForPair(string? input, out AssignmentForPair? value)
+{
+    value = null;
+
+    if (input == null) return false;
+
+    Regex numRegex = new(@"\d+");
+
+    var numbers = numRegex.Matches(input).Select(w => int.Parse(w.Value)).ToArray();
+
+    value = new AssignmentForPair(numbers[0], numbers[1], numbers[2], numbers[3]);
+
+    return true;
+}
+
+record AssignmentForPair(int Start1, int End1, int Start2, int End2)
+{
+    public bool IsOneFullyContainedByOther()
+    {
+        return Contains(Start1, End1, Start2, End2) ||
+            Contains(Start2, End2, Start1, End1);
+
+        static bool Contains(int start, int end, int otherStart, int otherEnd)
+        {
+            return start <= otherStart && end >= otherEnd;
+        }
+    }
+
+    public bool DoOverlapAtAll()
+    {
+        return IsPointOnLine(Start1, Start2, End2) ||
+            IsPointOnLine(End1, Start2, End2) ||
+            IsPointOnLine(Start2, Start1, End1) ||
+            IsPointOnLine(End2, Start1, End1);
+
+        static bool IsPointOnLine(int point, int start, int end)
+            => point >= start && point <= end;
+    }
+}
