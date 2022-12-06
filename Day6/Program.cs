4e3ac3a66dd685b9c6c0c051c18cebe373070750
David Božjak <bozjak@gmail.com>
2022-12-06 06:30:00 +0100

    Rewrote by using string ranges

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -1,32 +1,23 @@
 ﻿var input = new StringInputProvider("Input.txt").First();
 
-for (int i = 3; i < input.Length; i++)
+for (int i = 4; i < input.Length; i++)
 {
-    var map = new HashSet<char>();
-    map.Add(input[i]);
-    map.Add(input[i-1]);
-    map.Add(input[i - 2]);
-    map.Add(input[i - 3]);
+    var map = input[(i - 4)..i].ToHashSet();
 
     if (map.Count == 4)
     {
-        Console.WriteLine($"Part 1: {i+1}");
+        Console.WriteLine($"Part 1: {i}");
         break;
     }
 }
 
-for (int i = 13; i < input.Length; i++)
+for (int i = 14; i < input.Length; i++)
 {
-    var map = new HashSet<char>();
-    
-    for (int j = 13; j >= 0; j--)
-    {
-        map.Add(input[i - j]);
-    }
-    
+    var map = input[(i - 14)..i].ToHashSet();
+
     if (map.Count == 14)
     {
-        Console.WriteLine($"Part 2: {i + 1}");
+        Console.WriteLine($"Part 2: {i}");
         break;
     }
 }
